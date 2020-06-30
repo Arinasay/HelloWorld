e5b12ee9971bbcf49a6ecbb9e9bf8519761fb092
Arina <arina_rina_88@mail.ru>
2020-06-30 14:23:11 +0300

    change file hw.cpp

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -6,9 +6,11 @@
 
 
 using namespace std;
-/*
-	int a, b, c, d, num=0, den=0;4
-	char op = '+', dr = '/';
+
+int main()
+{
+	int a, b, c, d, num = 0, den = 0;
+		char op = '+', dr = '/';
 
 	cin >> a >> b >> c >> d;
 
@@ -17,17 +19,8 @@ using namespace std;
 	num = a * d + b * c;
 	den = b * d;
 
-	cout << num << dr << den << endl;5
-	den = b * d;
-	*/
-	//c = a / b;
-	//c = a ^ b;
-	//cout << c << endl;
-//for (size_t i = 0; i < length; i++)
-
-int main()
-{
-	int a = 7;
+	cout << num << dr << den << endl;
+		den = b * d;
 
 	return 0;
 }
